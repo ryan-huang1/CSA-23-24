2929b4015876e57a0c1ad622ebb7b8d38b3a8c7e
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2023-09-18 11:26:46 -0400

    Create YourNameHelloWorld.java

---
+++ Module-Three/Lesson Six/YourNameHelloWorld.java
@@ -0,0 +1,7 @@
+public class YourNameHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("\nSeneca Valley High School");  // Added new line before high school name
+        System.out.println("I'm a \"Screaming Eagle\"!\t\tGo Eagles!");  // Added mascot name in quotes, and tabbed twice
+    }
+}
