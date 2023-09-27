05aa125f3baeb669efd71d16d7e7aecc94b79338
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2023-09-26 21:16:05 -0400

    Create TextScanner.java

---
+++ Module-Four/TextScanner.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class TextScanner {
+
+    public static void main(String args[]) {
+        Scanner kbReader = new Scanner(System.in);
+        System.out.print("Enter your String here: "); //Enter One Two Three
+        String s = kbReader.nextLine();
+        System.out.println(s);
+    }
+}
