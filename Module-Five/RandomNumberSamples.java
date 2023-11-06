fc151ba236cd14e1ef7f1d79f424a988b5ffb8f5
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2023-11-05 19:22:24 -0500

    Create RandomNumberSamples.java

---
+++ Module-Five/RandomNumberSamples.java
@@ -0,0 +1,49 @@
+// import java.util.*;
+// public class Tester
+// {
+//     public static void main(String args[])
+//     {
+//         Random rnd = new Random();
+        
+//         for(int j = 0; j < 100; j++)
+//         {
+//             System.out.println(rnd.nextDouble());
+//         }
+//     }
+// }
+
+// import java.util.*;
+// public class Tester
+// {
+//     public static void main(String args[])
+//     {
+//         Random rnd = new Random();
+//         int rInt = 0;
+        
+//         for(int j = 0; j < 100; j++)
+//         {
+//             // Generate numbers in the range from 90 to 110 inclusive
+//             // 110 - 90 = 20
+//             rInt = 90 + rnd.nextInt(21);
+//             System.out.println(rInt);
+//         }
+//     }
+// }
+
+// import java.util.*;
+// public class Tester
+// {
+//     public static void main(String args[])
+//     {
+//         Random rnd = new Random();
+//         double d = 0;
+        
+//         for(int j = 0; j < 100; j++)
+//         {
+//             // Generate doubles in the range from 34.7838(inclusive) to 187.056(exclusive)
+//             // 187.056 - 34.7838 = 152.2722
+//             d = 34.7838 + 152.2722 * rnd.nextDouble();
+//             System.out.println(d);
+//         }
+//     }
+// }
