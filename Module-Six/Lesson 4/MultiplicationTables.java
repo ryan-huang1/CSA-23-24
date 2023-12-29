387d14bfbbe6863f21da786cb4def97d3d260f00
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2023-12-29 12:42:53 +0100

    comment the code

--- Module-Six/Lesson 4/MultiplicationTables.java
+++ Module-Six/Lesson 4/MultiplicationTables.java
@@ -1,12 +1,19 @@
 public class MultiplicationTables {
+    // Main method - entry point of the Java application
     public static void main(String[] args) {
+        // Print the title of the tables
         System.out.println("Tables");
 
-        for (int i = 1; i <= 12; i++) { // Outer loop for each table
-            for (int j = 1; j <= 12; j++) { // Inner loop for each multiplication
+        // Outer loop: Iterates over numbers 1 through 12 for each multiplication table
+        for (int i = 1; i <= 12; i++) {
+            // Inner loop: Iterates over numbers 1 through 12 for each entry in the current table
+            for (int j = 1; j <= 12; j++) {
+                // Print the multiplication result in the format "i * j = result"
                 System.out.println(i + " * " + j + " = " + (i * j));
             }
-            System.out.println(); // New line after each table
+            // Print a blank line for better readability, separating each table
+            System.out.println();
         }
     }
+    // No additional methods or classes are used in this simple program
 }
