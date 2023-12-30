e10573f88df5f5c38faff768c76d655ed2011229
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2023-12-30 07:57:42 +0100

    finish lesson two

---
+++ Module-Nine/shapes/Circle.java
@@ -0,0 +1,36 @@
+public class Circle {
+    private double radius;
+
+    // Constructor with one argument for radius
+    public Circle(double r) {
+        this.radius = r;
+    }
+
+    // Method to set the radius
+    public void setRadius(double nr) {
+        this.radius = nr;
+    }
+
+    // Method to get the radius
+    public double getRadius() {
+        return this.radius;
+    }
+
+    // Equals method to compare this Circle to another object
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Circle circle = (Circle) obj;
+        return Double.compare(circle.radius, this.radius) == 0;
+    }
+
+    // Method to calculate the area of the circle
+    public double area() {
+        return Math.PI * this.radius * this.radius;
+    }
+
+    // Method to calculate the circumference of the circle
+    public double circumference() {
+        return 2 * Math.PI * this.radius;
+    }
+}
---
+++ Module-Nine/shapes/Tester.java
@@ -0,0 +1,9 @@
+package shapes;
+
+public class Tester {
+    public static void main(String[] args) {
+        Circle cir1 = new Circle(12.1);
+        Circle cir2 = new Circle(12.1);
+        System.out.println(cir1.equals(cir2));
+    }
+}
